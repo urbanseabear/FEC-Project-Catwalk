ff1e6c89c28ca333b4881681ccc5b74bb53f5a44
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-12 17:02:37 -0500

    setup sass and jest

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
     ],
   },
   resolve: {
