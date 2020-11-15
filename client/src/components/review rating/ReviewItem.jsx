72396d87fe92e2ae3685f3f06b27bec04d420506
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-15 12:02:51 -0500

    Added recommend, helpful, and response sections
    Aligned with left grid

--- client/src/components/review rating/ReviewItem.jsx
+++ client/src/components/review rating/ReviewItem.jsx
@@ -3,16 +3,41 @@ import StarRating from './StarRating.jsx';
 
 const ReviewItem = () => {
   return (
-    <div>
-      <span style={{ float: 'right' }}>User | November 21, 2020</span>
+    <div style={{ paddingTop: '14px' }}>
+      <span style={{ float: 'right', marginTop: '24px' }}>
+        User | November 21, 2020
+      </span>
       <StarRating />
       <h1>Review title with word-break truncation to prevent wrapping</h1>
-      <p>
+      <p style={{ paddingTop: '10px', paddingBottom: '10px' }}>
         This is some sample review body text. This is some sample review body
         text. This is some sample review body text. This is some sample review
         body text.
       </p>
-      <span>Helpful? Yes (0) | Report</span>
+      <div>
+        <span>√ I recommend this product</span>
+      </div>
+      <div
+        style={{
+          border: 'solid',
+          borderColor: 'grey',
+          height: '70px',
+          margin: '40px 0px',
+          background: 'grey',
+        }}>
+        <div style={{ margin: '10px' }}>
+          <span> Response:</span>
+        </div>
+        <div style={{ margin: '10px' }}>
+          <span>
+            Marizpan danish jelly beans gummi beats apple pie cheesecake topping
+            biscuit sesame snaps.
+          </span>
+        </div>
+      </div>
+      <div>
+        <span>Helpful? Yes (0) | Report</span>
+      </div>
       <div
         style={{
           paddingTop: '30px',
