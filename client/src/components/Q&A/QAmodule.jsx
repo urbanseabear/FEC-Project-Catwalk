415ea5f4da3c58f654699cc3a0f39d34bb01952a
urbanseabear <calvin.rudy@gmail.com>
2020-11-15 17:23:22 -0500

    removed grid colors

--- client/src/components/Q&A/QAmodule.jsx
+++ client/src/components/Q&A/QAmodule.jsx
@@ -16,15 +16,15 @@ class QAmodule extends React.Component {
     render() {
         return (
             <div className='qa-container' style={{display: 'grid', gridTemplateRows: '.5fr 1fr 3fr 1 fr', gridTemplateColumns: '3fr .5fr .5fr 1fr', gap: '10px', marginLeft: '50px', marginRight: '50px',  fontFamily: 'Arial, Helvetica, sans-serif'}}>
-                <div className='qa-header' style={{gridColumnEnd: 'span 4', fontSize: '20px', backgroundColor: 'red', height: '30px'}} >QUESTIONS & ANSWERS</div>
+                <div className='qa-header' style={{gridColumnEnd: 'span 4', fontSize: '20px', height: '30px'}} >QUESTIONS & ANSWERS</div>
                 <QAsearch />
                 <QAlist answers={this.state.answers} questions={this.state.questions.results}/>
-                <div style={{gridColumnStart: '4', backgroundColor: 'green'}}>
+                <div style={{gridColumnStart: '4'}}>
                 <Helpful helped={this.state.questions.results[0].question_helpfulness} reportOrAdd={'Add Answer'}/>
                 <div style={{marginTop: '50px', marginBottom: '50px'}}></div>
                 <Helpful helped={this.state.questions.results[1].question_helpfulness} reportOrAdd={'Add Answer'}/>
                 </div>
-                <div style={{gridRowStart: '4', gridColumnEnd: 'span 4', backgroundColor: 'gray'}}>
+                <div style={{gridRowStart: '4', gridColumnEnd: 'span 4'}}>
                     <button style={{fontWeight: 'bold', fontSize: '18px', borderWidth: '2px', borderColor: 'black', background: 'none', padding: '15px 5px 15px 5px', cursor: 'pointer'}}>MORE ANSWERED QUESTIONS</button>
                     <button style={{fontWeight: 'bold', fontSize: '18px', marginLeft: '20px', borderWidth: '2px', borderColor: 'black', background: 'none', padding: '15px 5px 15px 5px', cursor: 'pointer'}}>ADD A QUESTION +</button>
                 </div>
--- client/src/components/Q&A/QAsearch.jsx
+++ client/src/components/Q&A/QAsearch.jsx
@@ -3,7 +3,7 @@ import '../../styles/main.scss';
 
 const QAsearch = (props) => {
     return (
-        <div style={{gridRowStart: '2', gridColumnStart: 'span 4', backgroundColor: 'blue'}}>
+        <div style={{gridRowStart: '2', gridColumnStart: 'span 4'}}>
             <input style={{ width: '98%', paddingBottom: '10px', paddingTop: '10px', fontWeight: 'bold', fontSize: '20px'}} className='qa-search-bar' type='text' placeholder="Have a question? Search for answers..."></input>
         </div>
     )
