9e0dc6926eebf3273b802095c1ff18a9ee7be555
urbanseabear <calvin.rudy@gmail.com>
2020-11-23 10:07:45 -0500

    more style refactor

--- client/src/components/Q&A/QAsearch.jsx
+++ client/src/components/Q&A/QAsearch.jsx
@@ -7,23 +7,10 @@ const QAsearch = (props) => {
     <div className="qa-search">
       <input
         onChange={(e) => props.search(e)} 
-        style={{
-          
-          width: "95%",
-          paddingLeft: "10px",
-          paddingBottom: "15px",
-          paddingTop: "15px",
-          fontWeight: "bold",
-          fontSize: "20px",
-          borderWidth: '4px',
-          borderColor: 'black',
-          borderStyle: 'solid none solid solid',
-          outline: 'none'
-        }}
         className="qa-search-bar"
         type="text"
         placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."
-      ></input><SearchIcon style={{padding: '10px 5px 10px 1px', fontSize: '40px', borderStyle: 'solid solid solid none'}}/>
+      ></input><SearchIcon id="searchIcon"/>
     </div>
   );
 };
