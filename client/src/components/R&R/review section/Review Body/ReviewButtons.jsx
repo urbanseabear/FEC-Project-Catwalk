e56fc2ba3acc300a2e1a51c572ca20b6e8693e3b
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-24 16:25:58 -0500

    added click handler

--- client/src/components/R&R/review section/Review Body/ReviewButtons.jsx
+++ client/src/components/R&R/review section/Review Body/ReviewButtons.jsx
@@ -2,13 +2,25 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import AnimatedModal from '../Modal/Modal';
 
-const ReviewButtons = ({ metaData, click }) => {
+const ReviewButtons = ({ metaData, click, data }) => {
+  let noMoreItems = data.length !== 0;
+
+  let moreReviews = () => {
+    return (
+      <div style={{ paddingRight: '30px' }}>
+        <Button
+          style={{ fontSize: '25px' }}
+          variant='contained'
+          onClick={() => click()}>
+          MORE REVIEWS
+        </Button>
+      </div>
+    );
+  };
+
   return (
     <div style={{ display: 'flex', marginTop: '10px' }}>
-      <Button style={{ fontSize: '25px' }} variant='contained' onClick={click}>
-        MORE REVIEWS
-      </Button>
-      <div style={{ paddingRight: '30px' }}></div>
+      {noMoreItems ? moreReviews() : ''}
       <AnimatedModal metaData={metaData} />
     </div>
   );
