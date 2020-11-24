a169f5a2fbe00bff2451711d78df9e011f74ba40
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-24 11:15:45 -0500

    finished char rating

--- client/src/components/R&R/summary section/CharRatingList.jsx
+++ client/src/components/R&R/summary section/CharRatingList.jsx
@@ -4,6 +4,18 @@ import { charRating } from '../utils/helper.js';
 
 const CharRatingList = ({ metaData }) => {
   let features = Object.keys(metaData);
+  let values = Object.values(metaData);
+  let result = [];
+  let count = 0;
+
+  values.map((el) => {
+    let first = el.value[0];
+    let second = el.value.slice(1, 3);
+
+    let val = parseInt(first) * 20 + parseFloat(second) * 20;
+
+    result.push((val * 3).toString());
+  });
 
   const list = features.map((el) => {
     let titleLabel = el;
@@ -17,6 +29,7 @@ const CharRatingList = ({ metaData }) => {
           leftLabel={charObj.L}
           middleLabel={charObj.M}
           rightLabel={charObj.R}
+          value={result[count++]}
         />
       </div>
     );
