16390016ee4e8b6b0d80065680c80122cef52113
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-24 11:12:42 -0500

    added event handler to button

--- client/src/components/R&R/review section/Review Body/ReviewButtons.jsx
+++ client/src/components/R&R/review section/Review Body/ReviewButtons.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import AnimatedModal from '../Modal/Modal';
 
-const ReviewButtons = ({ metaData }) => {
+const ReviewButtons = ({ metaData, click }) => {
   return (
     <div style={{ display: 'flex', marginTop: '10px' }}>
-      <Button style={{ fontSize: '25px' }} variant='contained'>
+      <Button style={{ fontSize: '25px' }} variant='contained' onClick={click}>
         MORE REVIEWS
       </Button>
       <div style={{ paddingRight: '30px' }}></div>
