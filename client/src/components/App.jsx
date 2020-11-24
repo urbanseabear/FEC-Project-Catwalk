031318acb1ddef08913f0365408298175d70d30c
urbanseabear <calvin.rudy@gmail.com>
2020-11-24 12:43:05 -0500

    react router functioning

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,16 +4,26 @@ import Overview from './overview/overview.jsx';
 import QAmodule from './Q&A/QAmodule';
 import ReviewRatings from './R&R/ReviewRatings';
 import Grid from '@material-ui/core/Grid';
+import {Route, generatePath, useHistory, Switch} from 'react-router-dom';
 
 const App = () => {
   const [productId, setProductId] = useState(14);
   const [page, setPage] = useState(1);
   const [count, setCount] = useState(100);
+  const hist = useHistory();
 
   const onSearch = (productId) => {
     setProductId(productId);
+    updatePath(productId);
   };
+
+  const updatePath = (productId) => {
+    const path = generatePath("/product/:id", {id: productId});
+    hist.push(path);
+  }
   return (
+    <Switch>
+    <Route exact path="/">
     <div>
       <Grid container spacing={8}>
         <Grid item xs={12}>
@@ -27,6 +37,23 @@ const App = () => {
         </Grid>
       </Grid>
     </div>
+    </Route>
+    <Route exact path="/product/:id">
+    <div>
+      <Grid container spacing={8}>
+        <Grid item xs={12}>
+          <Overview productId={productId} onSearch={onSearch} />
+        </Grid>
+        <Grid style={{ margin: '0% 10%' }} item xs={12}>
+          <QAmodule prodID={productId} />
+        </Grid>
+        <Grid style={{ margin: '0% 10%' }} item xs={12}>
+          <ReviewRatings productId={productId} page={page} count={count} />
+        </Grid>
+      </Grid>
+    </div>
+    </Route>
+    </Switch>
   );
 };
 
