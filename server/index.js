80bd00a38fcd459fb825174785c322ac3165e514
dylanallgood <dylanblakeallgood@gmail.com>
2020-11-29 16:51:54 -0500

    worked on darkmode

--- server/index.js
+++ server/index.js
@@ -6,8 +6,7 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 const mainDir = __dirname.substring(0, __dirname.length - 6);
-console.log(mainDir)
-
+console.log(mainDir);
 
 // middleware
 app.use(parser.json());
@@ -15,9 +14,8 @@ app.use(parser.urlencoded({ extended: true }));
 app.use(express.static('./client/dist'));
 app.use(cors());
 
-
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(mainDir, 'client/dist', 'index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(mainDir, 'client/dist', 'index.html'));
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
